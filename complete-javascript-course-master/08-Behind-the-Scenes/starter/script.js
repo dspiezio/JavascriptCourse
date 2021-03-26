3d17931d648b6646f86954de737ca5e93eb80bf2
dspiezio <dspiezio@gmail.com>
2021-03-26 12:48:13 -0400

    End of lesson

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,106 @@
 'use strict';
+// const currentYear = 2021;
+// const firstName = 'Dominic';
+
+// function calcAge(birthYear) {
+//   const age = currentYear - birthYear;
+
+//   function printAge() {
+//     const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
+
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const str = `Oh, and you are a millenial, ${firstName}`;
+//       console.log(str);
+
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     console.log(millenial);
+//     // console.log(add(2, 3));
+//   }
+
+//   printAge();
+//   return age;
+// }
+
+// calcAge(1988);
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = `Dominic`;
+// let job = `Worker`;
+// const year = 1988;
+
+// console.log(addDecl(2, 3));
+// console.log(addExpr(4, 4));
+// console.log(addArrow(3, 5));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(currentYear - birthYear);
+//   console.log(this);
+// };
+
+// const calcAgeArrow = birthYear => {
+//   console.log(currentYear - birthYear);
+//   console.log(this);
+// };
+
+// var firstName = 'Lydia';
+
+// dominic.greet();
+// const lydia = {
+//   year: 1994,
+// };
+
+// dominic.calcAge();
+// lydia.calcAge = dominic.calcAge;
+// const f = dominic.calcAge;
+// console.log(lydia);
+// lydia.calcAge();
+
+// calcAge(1988);
+// calcAgeArrow(1988);
+
+// const currentYear = 2021;
+// const dominic = {
+//   firstName: 'Dominic',
+//   year: 1988,
+//   calcAge: function () {
+//     console.log(currentYear - this.year);
+//   },
+// };
+
+// let age = 33;
+// console.log(age);
+// let oldAge = age;
+// age = 34;
+// console.log(oldAge);
+// console.log(age);
+
+// const me = {
+//   name: 'Dominic',
+//   age: 33,
+// };
+// const friend = me;
+// console.log('Me', me);
+// friend.age = 26;
+// console.log('Friend:', friend);
+
+let lastName = 'Spiezio';
+let oldLastName = lastName;
